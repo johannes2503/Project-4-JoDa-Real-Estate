3ddf6926472c1b60e3a62562ceea6ee8a0bb8671
Cryo79 <johannes2503@gmail.com>
2022-11-23 14:22:09 +0000

    Add search function to form and preserve input

--- listings/views.py
+++ listings/views.py
@@ -23,11 +23,34 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+     # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
 
     context = {
-
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
+        'listings': queryset_list,
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', context)
