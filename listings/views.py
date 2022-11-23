c324b2e47a87dc2b4b702c0bbb9e9600ee500c0a
Cryo79 <johannes2503@gmail.com>
2022-11-23 10:11:44 +0000

    Add latest listings to home page

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,7 @@ from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     context = {
         'listings': listings
     }
@@ -12,7 +12,7 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from listings.models import Listing
+
+
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:2]
+
+    context = {
+        'listings': listings,
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
     return render(request, 'pages/about.html')
-
-
