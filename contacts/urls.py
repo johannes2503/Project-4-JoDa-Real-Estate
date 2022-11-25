77379487200d4751deff29c0377c97a63244c437
Cryo79 <johannes2503@gmail.com>
2022-11-25 13:09:07 +0000

    Work on edit inquiry function

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
-from django.core.mail import send_mail
 from .models import Contact
 
 
@@ -13,7 +12,6 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
         user_id = request.POST['user_id']
-        realtor_email = request.POST['realtor_email']
 
         #  Check if user has made inquiry already
         if request.user.is_authenticated:
@@ -29,15 +27,6 @@ def contact(request):
                           email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
-
-        # Send email
-        # send_mail(
-        #   'Property Listing Inquiry',
-        #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-        #   'traversy.brad@gmail.com',
-        #   [realtor_email, 'techguyinfo@gmail.com'],
-        #   fail_silently=False
-        # )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
