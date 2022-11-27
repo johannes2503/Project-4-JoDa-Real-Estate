54405ef6f30e2188b56f62cf0d814a9645193c4f
Cryo79 <johannes2503@gmail.com>
2022-11-27 16:01:05 +0000

    Debug not working

--- joda_real_estate/settings.py
+++ joda_real_estate/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['joda-real-estate.herokuapp.com', '127.0.0.1']
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
