0ff943e2928492828814a9a1f6d79ef71cb712f8
Cryo79 <johannes2503@gmail.com>
2023-01-04 21:28:07 +0000

    Trying static files config change

--- joda_real_estate/settings.py
+++ joda_real_estate/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
+STATICFILES_STORAGE = os.environ.get("STATIC_FILES_STORAGE")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
