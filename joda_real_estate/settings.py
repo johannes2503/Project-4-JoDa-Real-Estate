073aeab075f97ebe30ebc7f6cdb13bded9d00706
Cryo79 <johannes2503@gmail.com>
2023-01-04 19:18:34 +0000

    Add cloudinary path to static files

--- joda_real_estate/settings.py
+++ joda_real_estate/settings.py
@@ -128,14 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
